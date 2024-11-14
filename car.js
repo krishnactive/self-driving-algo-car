fa3b68f5ac620d8fd5d5d37f10c37cf0f58e2058
Krishna kant sharma <krishnactive@gmail.com>
2024-11-14 17:29:47 +0530

    car movement

--- car.js
+++ car.js
@@ -7,6 +7,8 @@ class Car{
 
         this.speed = 0;
         this.acceleration = 0.2;
+        this.maxSpeed = 3;
+        this.friction = 0.05;
 
         this.controls = new Controls();
     }
@@ -18,7 +20,22 @@ class Car{
         if(this.controls.reverse){
             this.speed-=this.acceleration;
         }
-        this.y -= this.speed;
+        if(this.speed>this.maxSpeed){
+            this.speed = this.maxSpeed;
+        }
+        if(this.speed<-this.maxSpeed/2){
+            this.speed = -this.maxSpeed/2;
+        }
+        if(this.speed>0){
+            this.speed-=this.friction;
+        }
+        if(this.speed<0){
+            this.speed+=this.friction;
+        }
+        if(Math.abs(this.speed)<this.friction){
+            this.speed = 0;
+        }
+        this.y-=this.speed;
         if(this.controls.left){
             this.x-=2;
         }

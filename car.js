e619dfee609c2c8a804ee2cff37665a69c16afc8
Krishna kant sharma <krishnactive@gmail.com>
2024-11-16 00:28:57 +0530

    adding artificial sensor done

--- car.js
+++ car.js
@@ -10,11 +10,16 @@ class Car{
         this.maxSpeed = 2.5;
         this.friction = 0.05;
         this.angle = 0;
+
+        this.sensor=new Sensor(this);
+
         this.controls = new Controls();
     }
 
-    update(){
+    update(roadBorders){
+    
         this.#move();
+        this.sensor.update(roadBorders);
     }
     #move(){
         //box2d library can be used
@@ -67,5 +72,7 @@ class Car{
         );
         ctx.fill();
         ctx.restore();
+
+        this.sensor.draw(ctx);
     }
 }
